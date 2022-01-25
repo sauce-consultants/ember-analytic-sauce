72fd1dbe522b58255a8debda84a2316da9e02379
Jim Wardlaw <jim@wearesauce.io>
2022-01-25 11:16:48 +0000

    Remove jQuery requirement

--- addon/services/analytics.js
+++ addon/services/analytics.js
@@ -1,18 +1,9 @@
-import Service from '@ember/service';
-import $ from 'jquery';
-import config from 'ember-get-config';
-import {
-  inject as service
-} from '@ember/service';
-import {
-  computed,
-  get,
-  set,
-} from '@ember/object';
-import {
-  not
-} from '@ember/object/computed';
-
+import Service from "@ember/service";
+import $ from "jquery";
+import config from "ember-get-config";
+import { inject as service } from "@ember/service";
+import { computed, get, set } from "@ember/object";
+import { not } from "@ember/object/computed";
 
 export default Service.extend({
   // Attributes
@@ -21,113 +12,113 @@ export default Service.extend({
   user: null,
   ip: null,
   // Services
-  browser: service(),
-  cordovaPlatform: service('ember-cordova/platform'),
+  cordovaPlatform: service("ember-cordova/platform"),
   // Computed
-  url: computed(function() {
-    const url = get(config, 'analytics-sauce.apiUrl');
+  url: computed(function () {
+    const url = get(config, "analytics-sauce.apiUrl");
     if (!url) {
       throw Error("please set a config value for analytics-sauce.apiUrl");
     }
     return url;
   }),
-  globalSequence: computed('viewSequence', 'eventSequence', function() {
-    return get(this, 'viewSequence') + get(this, 'eventSequence');
+  globalSequence: computed("viewSequence", "eventSequence", function () {
+    return get(this, "viewSequence") + get(this, "eventSequence");
   }),
-  isWeb: computed('cordovaPlatform.isCordova', function() {
-    return !this.get('cordovaPlatform.isCordova');
+  isWeb: computed("cordovaPlatform.isCordova", function () {
+    return !this.get("cordovaPlatform.isCordova");
   }),
-  isApp: not('isWeb'),
-  platform: computed('isWeb', 'isApp', function() {
-    if (get(this, 'isWeb')) {
-      return 'web';
+  isApp: not("isWeb"),
+  platform: computed("isWeb", "isApp", function () {
+    if (get(this, "isWeb")) {
+      return "web";
     }
-    if (get(this, 'isWeb')) {
-      return 'app';
+    if (get(this, "isWeb")) {
+      return "app";
     }
   }),
-  userAgent: computed(function() {
-    return (navigator.userAgent || navigator.vendor || window.opera);
+  userAgent: computed(function () {
+    return navigator.userAgent || navigator.vendor || window.opera;
   }),
-  iOS: computed('userAgent', function() {
-    const userAgent = this.get('userAgent');
-    return (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i));
+  iOS: computed("userAgent", function () {
+    const userAgent = this.get("userAgent");
+    return (
+      userAgent.match(/iPad/i) ||
+      userAgent.match(/iPhone/i) ||
+      userAgent.match(/iPod/i)
+    );
   }),
-  android: computed('userAgent', function() {
-    const userAgent = this.get('userAgent');
-    return (userAgent.match(/Android/i));
+  android: computed("userAgent", function () {
+    const userAgent = this.get("userAgent");
+    return userAgent.match(/Android/i);
   }),
   // Methods
   init() {
     this._super(...arguments);
     // create session hash
-    const session = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    set(this, 'session', session);
+    const session =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+    set(this, "session", session);
   },
   setUser(user) {
-    set(this, 'user', user);
+    set(this, "user", user);
   },
   baseProperties() {
     const row = {};
     // ember app details
-    set(row, 'environment', get(config, 'environment'));
-    set(row, 'appName', get(config, 'APP.name'));
+    set(row, "environment", get(config, "environment"));
+    set(row, "appName", get(config, "APP.name"));
     // package version
-    const v = get(config, 'APP.version').split('+');
-    set(row, 'appVersion', v[0]);
-    set(row, 'appHash', v[1]);
+    const v = get(config, "APP.version").split("+");
+    set(row, "appVersion", v[0]);
+    set(row, "appHash", v[1]);
     // System setting
-    set(row, 'userAgent', window.navigator.userAgent);
+    set(row, "userAgent", window.navigator.userAgent);
     // User details
-    set(row, 'sessionId', get(this, 'session'));
-    set(row, 'userId', get(this, 'user'));
+    set(row, "sessionId", get(this, "session"));
+    set(row, "userId", get(this, "user"));
     return row;
   },
   trackVisit(view, title) {
-
-    this.log('Track View');
+    this.log("Track View");
 
     const row = this.baseProperties();
 
-    this.incrementProperty('viewSequence');
-    set(row, 'viewSequence', get(this, 'viewSequence'));
-    set(row, 'globalSequence', get(this, 'globalSequence'));
+    this.incrementProperty("viewSequence");
+    set(row, "viewSequence", get(this, "viewSequence"));
+    set(row, "globalSequence", get(this, "globalSequence"));
 
-    set(row, 'name', view);
-    set(row, 'title', title);
+    set(row, "name", view);
+    set(row, "title", title);
 
-    return this.send('/visits', row);
+    return this.send("/visits", row);
   },
   trackEvent(event, data) {
-    this.log('Track Event');
+    this.log("Track Event");
 
     const row = this.baseProperties();
 
-    this.incrementProperty('eventSequence');
-    set(row, 'eventSequence', get(this, 'eventSequence'));
-    set(row, 'globalSequence', get(this, 'globalSequence'));
+    this.incrementProperty("eventSequence");
+    set(row, "eventSequence", get(this, "eventSequence"));
+    set(row, "globalSequence", get(this, "globalSequence"));
 
-    set(row, 'name', event);
-    set(row, 'data', data);
+    set(row, "name", event);
+    set(row, "data", data);
 
-    return this.send('/events', row);
+    return this.send("/events", row);
   },
   send(uri, data) {
-
-    const url = `${get(this, 'url')}${uri}`;
+    const url = `${get(this, "url")}${uri}`;
 
     data = JSON.stringify(data);
 
     if (this.shouldSendData()) {
-
-      return $.ajax({
-        type: "POST",
-        url,
-        data,
-        success: (data) => {
-          this.log('analytics sent', data);
-        },
-        dataType: 'json'
+      return fetch(url, {
+        method: "POST",
+        headers: { accept: "application/json" },
+        body: data,
+      }).then((response) => {
+        this.log("analytics sent", data);
       });
     } else {
       this.log(url, data);
@@ -135,7 +126,7 @@ export default Service.extend({
   },
   shouldSendData() {
     const currentEnv = config.environment,
-      activeEnvs = get(config, 'analytics-sauce.environments');
+      activeEnvs = get(config, "analytics-sauce.environments");
 
     if (activeEnvs) {
       return activeEnvs.indexOf(currentEnv) !== -1;
@@ -143,8 +134,8 @@ export default Service.extend({
     return false;
   },
   log(...args) {
-    if (get(config, 'analytics-sauce.debug')) {
+    if (get(config, "analytics-sauce.debug")) {
       window.console.log(...args);
     }
-  }
+  },
 });
